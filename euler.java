32087d95bbb9cd152181b5c8aee96ce6ed475083
tyler <ttang7910@bths.edu>
2024-01-11 17:37:33 -0500

    wrote outline for eulers method

---
+++ euler.java
@@ -0,0 +1,12 @@
+public class euler {
+    public static void main(String[] args) {
+        // y_n = y_n-1 + f'(x_n-1 , y_n-1);
+
+
+        int y = 
+
+        for(int i = 0; i < n; i++) {
+
+        }
+    }
+}
