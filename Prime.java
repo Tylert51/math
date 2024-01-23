9ffe9a046033f0445e5b073292a79287b0c511ed
tyler <ttang7910@bths.edu>
2024-01-22 22:27:01 -0500

    wrote algoritm to genearate primes

---
+++ Prime.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Prime {
+    public static void main(String[] args) {
+        int start = 3; 
+        int end = 1000000;
+
+        ArrayList<Integer> primes = new ArrayList<>();
+        primes.add(2);
+
+
+
+        for (int i = start; i <= end; i += 2) {
+
+            boolean p = true;
+
+            for(int x : primes) {
+
+                if (x <= Math.sqrt(i) + 1) {
+
+                    if(i % x == 0) {
+                        p = false;
+                    } 
+
+                } else {
+                    break;
+                }
+            }
+        
+
+            if(p) {
+                primes.add(i);
+            }
+
+        }
+
+        System.out.println(primes);
+        System.out.println(primes.size());
+    }
+}
